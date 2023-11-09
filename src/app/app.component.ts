5e723e653e2b89a8fc1bfbbd37ca2ad85ac67f3a
Chris Darden <chris.a.darden@gmail.com>
2023-11-08 23:20:55 -0600

    finished project 2, rules to override lowercase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,30 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  username: string = "Chris";
+  allowNewServer: boolean = false;
+  userName: string = "";
   
-  getUsername(): string { return this.username; } 
+  getUsername(): string {
+    return this.userName;
+  }
+
+  constructor () { 
+
+}
+
+onUpdateUserName(event: any) {
+  this.userName = (<HTMLInputElement>event.target).innerHTML = "";
+  this.userName = (<HTMLInputElement>event.target).value; 
+}
+
+resetUserNameButtonClick(event: any) {
+  console.log("clicked");
+  console.log(event)
+  // this.userName = (<HTMLInputElement>event.target).innerHTML = "";
+  this.userName = "";
+
+}
+
 }
--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,6 +9,7 @@ import { Component } from "@angular/core";
 export class ServerComponent {
     serverId: number = 10;
     serverStatus: string = "offline";
+    allowNewServer: boolean = false;
 
     getServerStatus(): string {
         return this.serverStatus;

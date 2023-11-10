d65ce118950239052f8a109cb7210f844a51ce6a
Chris Darden <chris.a.darden@gmail.com>
2023-11-09 19:01:38 -0600

    working on assignment 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { Button } from "protractor";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  username: string = "Chris";
+  allowNewServer: boolean = false;
+  userName: string = "";
+  clicks = [];
+  buttonClickedStatus: boolean = false;
   
-  getUsername(): string { return this.username; } 
+  getUsername(): string {
+    return this.userName;
+  }
+
+  constructor () { 
+    this.buttonClickedStatus;
+    console.log("Button clicked: " + this.buttonClickedStatus);
+}
+
+onUpdateUserName(event: any) {
+  this.userName = (<HTMLInputElement>event.target).innerHTML = "";
+  this.userName = (<HTMLInputElement>event.target).value; 
+}
+
+resetUserNameButtonClick(event: any) {
+  console.log("clicked");
+  console.log(event)
+  // this.userName = (<HTMLInputElement>event.target).innerHTML = "";
+  this.userName = "";
+
+}
+
+buttonClicked(event: any) {
+  console.log("clicked");
+  this.buttonClickedStatus = true;
+  console.log(<HTMLInputElement>event);
+  console.log("button clicked status = " + this.buttonClickedStatus);
+  return this.buttonClickedStatus === true ? "block" : "none";
+  
+
+}
+
 }
